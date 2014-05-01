cdefdc74f6324678d5c8c1cc14633f0273536dc6
Jethro CM Kim <jethro.cm.kim@gmail.com>
2014-05-01 14:01:12 -0700

    headline font size is responsive. removed headline link and animation on contact information

--- public/js/client_side.js
+++ public/js/client_side.js
@@ -2,4 +2,7 @@ io=io.connect();
 
 $(document).ready(function() {
 
+	$('#name').fitText();
+	
+
 });
--- public/js/custom.js
+++ public/js/custom.js
@@ -2,8 +2,10 @@
 	$( '#dl-menu' ).dlmenu();
 	$('ul.dl-menu li a').smoothScroll();
 
+	$('#name').fitText(1.1, { minFontSize: '28px', maxFontSize: '48px' });
+
+	$('#subtitle').fitText(1.5, { minFontSize: '10px', maxFontSize: '24px' });
 
 	//animation
 	new WOW().init();
-
 })(jQuery);
